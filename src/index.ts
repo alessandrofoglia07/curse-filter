dee539d1d4f00101ebfa0ae187e2b72bf3a7c823
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-07-29 23:00:01 +0200

    init commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import af from './langs/af.js';
+import cz from './langs/cz.js';
+import de from './langs/de.js';
+import en from './langs/en.js';
+import es from './langs/es.js';
+import fi from './langs/fi.js';
+import fr from './langs/fr.js';
+import hi from './langs/hi.js';
+import it from './langs/it.js';
+import nl from './langs/nl.js';
+import pl from './langs/pl.js';
+import ru from './langs/ru.js';
+import sk from './langs/sk.js';
+import th from './langs/th.js';
+import uk from './langs/uk.js';
+
+const supportedLangs = ['af', 'cz', 'de', 'en', 'es', 'fi', 'fr', 'hi', 'it', 'nl', 'pl', 'ru', 'sk', 'th', 'uk'];
+
